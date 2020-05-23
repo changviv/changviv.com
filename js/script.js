8fd5ce971887f9504f7bb923f15c78c00cf11ce3
Vivien <vchang103@gmail.com>
2020-05-22 18:42:51 -0700

    skills and stats

--- js/script.js
+++ js/script.js
@@ -15,4 +15,45 @@ $(document).ready(function() {
   };
 
   var typed = new Typed('.typed', options);
+
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    items: 5,
+    responsive:{
+      0:{
+          items:1
+      },
+      480:{
+          items:2
+      },
+      768:{
+          items:3
+      },
+      938:{
+          items:4
+      }
+    }
+  })
+
+
+  var skillsOffTopSet = $('.skillSection').offset().top;
+
+  $(window).scroll(function() {
+    if (window.pageYOffset > skillsOffTopSet - $(window).height() + 200) {
+
+      $('.chart').easyPieChart({
+        easing: "easeInOut",
+        trackColor: false,
+        barColor: '#fff',
+        scaleColor: false,
+        lineWidth: 6,
+        size: 152,
+        onStep: function(from,to,percent) {
+          $(this.el).find('.percent').text(Math.round(percent));
+        }
+      });
+
+    }
+  })
 });
